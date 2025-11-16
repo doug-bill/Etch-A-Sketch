98fa98df44d0ada1ef2fa44c5f311135c63abfcc
doug-bill <65959310+doug-bill@users.noreply.github.com>
2025-11-16 00:40:35 -0300

    Created the Etch a Sketch
    Created with the effect of pen on paper using mouse events.
    
    Challenge for improving:
    - add a eraser button.
    - Rainbow mode.
    - Create darkening effect.
    - Support for mobile (?).

--- logic.js
+++ logic.js
@@ -6,46 +6,72 @@ body.appendChild(labels);
 
 const btn = document.createElement("button");
 btn.classList.add("button");
-btn.textContent = "Create grid";
-labels.appendChild(btn);
+btn.textContent = "Create new grid";
+labels.appendChild(btn);89
 
 const container = document.createElement("div");
 container.classList.add("flex-container");
 container.style.margin = "auto";
-container.style.backgroundColor = "white";
-
 body.appendChild(container);
 
 function createGrid(num){
 
  for (let i = 0 ; i < num* num; i++){
-        
+  
   const cell = document.createElement("div");
   cell.classList.add("col");
-  cell.style.flexBasis = `${100 / num}%`;
-  container.appendChild(cell); 
-      
+  cell.style.flexBasis = `${100 / num}%`; 
+  container.appendChild(cell);     
   }
-
 }
 
 btn.addEventListener("click", () => {
 
 let newGrid = parseInt(window.prompt("Choose a grid size from 1 - 100:"));
 
-
-console.log(newGrid)
-
   if(!isNaN(newGrid) && newGrid > 0 && newGrid <= 100  ){
-  //  while (container.firstChild){
-  //  container.removeChild(container.firstChild);
-   //  }
-   container.innerHTML = "";
+     container.innerHTML = "";
      createGrid(newGrid);
-  }    
+     }    
+    
+});
+
+createGrid(16);
+
+let isDrawing = false;
+
+container.addEventListener("mousedown" ,(event) => {
+
+  isDrawing = true;
+  if (event.target.classList.contains("col") && isDrawing == true) {
+      event.target.style.backgroundColor = "turquoise"; 
+
+  container.addEventListener("mouseover" , (event)=>{
+
+   if (event.target.classList.contains("col") && isDrawing == true) {
+      event.target.style.backgroundColor = "turquoise"; 
+  } 
+});
+  
+
+  } 
 });
 
 
-createGrid(0);
-   
+body.addEventListener("mouseup", event => {
+    isDrawing = false;
 
+});
+
+
+
+
+  
+/*   const cell = event.target;
+  if (!cell.classList.contains("col")) return;
+
+  cell.style.backgroundColor = "rgb(" +
+    Math.floor(Math.random() * 255) + "," +
+    Math.floor(Math.random() * 255) + "," +
+    Math.floor(Math.random() * 255) + ")";
+ */
