3ac19e0b9565b527c7c0940f60ccf1cea7af9410
doug-bill <65959310+doug-bill@users.noreply.github.com>
2025-11-17 10:58:58 -0300

    Version 1.1 Done
    Adds

--- logic.js
+++ logic.js
@@ -1,18 +1,56 @@
+let isDrawing = false;
+let isErasing = false;
+let isMouseDown = false;
+
+let mode = "draw";
+
 const body = document.querySelector("body");
+
 
 const labels = document.createElement("div");
 labels.classList.add("labels");
 body.appendChild(labels);
 
-const btn = document.createElement("button");
-btn.classList.add("button");
-btn.textContent = "Create new grid";
-labels.appendChild(btn);89
+const gridBtn = document.createElement("button");
+gridBtn.classList.add("button");
+gridBtn.textContent = "Create new grid";
+labels.appendChild(gridBtn);
+
+const p = document.createElement("p");
+p.classList.add("p");
+p.textContent = "Modes: ";
+labels.appendChild(p);
+
+const drawBtn = document.createElement("button");
+drawBtn.classList.add("button");
+drawBtn.textContent = "Draw";
+labels.appendChild(drawBtn);
+
+const eraserBtn = document.createElement("button");
+eraserBtn.classList.add("button");
+eraserBtn.textContent = "Eraser";
+labels.appendChild(eraserBtn);
+
+const rainbowBtn = document.createElement("button");
+rainbowBtn.classList.add("button");
+rainbowBtn.textContent = "Rainbow";
+labels.appendChild(rainbowBtn);
+
+/* const darkenBtn = document.createElement("button");
+darkenBtn.classList.add("button");
+darkenBtn.textContent = "Darken";
+labels.appendChild(darkenBtn); */
+
+const resetBtn = document.createElement("button");
+resetBtn.classList.add("button");
+resetBtn.textContent = "Reset";
+labels.appendChild(resetBtn);
 
 const container = document.createElement("div");
 container.classList.add("flex-container");
 container.style.margin = "auto";
 body.appendChild(container);
+
 
 function createGrid(num){
 
@@ -25,7 +63,7 @@ function createGrid(num){
   }
 }
 
-btn.addEventListener("click", () => {
+gridBtn.addEventListener("click", () => {
 
 let newGrid = parseInt(window.prompt("Choose a grid size from 1 - 100:"));
 
@@ -38,33 +76,111 @@ let newGrid = parseInt(window.prompt("Choose a grid size from 1 - 100:"));
 
 createGrid(16);
 
-let isDrawing = false;
+
+// Drawing/Erasing/Painting Logic -----------
+
+drawBtn.addEventListener("click", () => {
+  mode = "draw";
+});
+
+eraserBtn.addEventListener("click", () => {
+  mode = "erase";
+});
+
+rainbowBtn.addEventListener("click",() =>{
+  mode = "rainbow";
+});
+
+/* darkenBtn.addEventListener("click", () => {
+  mode = "darken";
+  LightenDarkenColor("40e0d0", -10);
+  console.log(LightenDarkenColor("40e0d0", -10));
+});
+ */
+resetBtn.addEventListener("click",() => {
+
+container.innerHTML = "";
+createGrid(16);
+
+});
 
 container.addEventListener("mousedown" ,(event) => {
 
-  isDrawing = true;
-  if (event.target.classList.contains("col") && isDrawing == true) {
-      event.target.style.backgroundColor = "turquoise"; 
+ isMouseDown = true;
 
-      container.addEventListener("mouseover" , (event)=>{
+   if (!event.target.classList.contains("col")) return;
 
-       if (event.target.classList.contains("col") && isDrawing == true) {
-        event.target.style.backgroundColor = "turquoise"; 
-      }});
+    if(mode === "draw"){
+      event.target.style.backgroundColor ="turquoise";
+    }
 
-  }});
-
-body.addEventListener("mouseup", event => {
-    isDrawing = false;
-});
-
-
-// for the Raibow --- try testing HSLV
-/*   const cell = event.target;
-  if (!cell.classList.contains("col")) return;
-
-  cell.style.backgroundColor = "rgb(" +
+    else if(mode === "rainbow"){
+    event.target.style.backgroundColor = "rgb(" +
     Math.floor(Math.random() * 255) + "," +
     Math.floor(Math.random() * 255) + "," +
     Math.floor(Math.random() * 255) + ")";
- */
+
+    }/* 
+    else if (mode === "darken"){
+    const newColor = LightenDarkenColor("40e0d0", -10);
+      event.target.style.backgroundColor = "#"+ {newColor};
+
+    } */
+
+    else if(mode === "erase"){
+      event.target.style.backgroundColor ="blueviolet";
+    }
+});
+
+
+body.addEventListener("mouseup", event => {
+    isMouseDown = false;
+});
+
+
+ container.addEventListener("mouseover" , (event)=>{
+
+  if(!isMouseDown) return;
+  if(!event.target.classList.contains("col")) return;
+  
+  if (mode === "draw"){
+     event.target.style.backgroundColor = "turquoise";
+  }
+  else if(mode === "rainbow"){
+
+    event.target.style.backgroundColor = "rgb(" +
+    Math.floor(Math.random() * 255) + "," +
+    Math.floor(Math.random() * 255) + "," +
+    Math.floor(Math.random() * 255) + ")";
+  }
+
+  else if (mode === "erase"){
+   event.target.style.backgroundColor = "blueviolet"; 
+  }
+
+});
+
+// FOOTER with github link
+
+const footer = document.createElement("footer");
+footer.classList.add("footer");
+footer.classList.add("a");
+footer.href = "https://github.com/doug-bill";
+footer.textContent = "Developed by Doug-bill";
+body.appendChild(footer);
+
+
+const githubLink = document.createElement("a");
+githubLink.href = "https://github.com/doug-bill";
+githubLink.target = "_blank"; 
+githubLink.rel = "noopener noreferrer";
+
+// Logo do GitHub
+const githubLogo = document.createElement("img");
+githubLogo.src = "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg";
+githubLogo.alt = "GitHub Logo";
+githubLogo.classList.add("github-logo");
+
+githubLink.appendChild(githubLogo);
+footer.appendChild(githubLink);
+
